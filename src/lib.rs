494f00e14e46a759985bc90ad4d737033e242141
David Roundy <roundyd@physics.oregonstate.edu>
2018-07-13 11:32:40 -0700

    slightly more compact code

--- src/lib.rs
+++ src/lib.rs
@@ -164,9 +164,7 @@ impl<T> IndexMut<usize> for Vector3d<T> {
 }
 
 use std::fmt;
-impl<T> fmt::Display for Vector3d<T>
-    where T: fmt::Display
-{
+impl<T: fmd::Display> fmt::Display for Vector3d<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "({}, {}, {})", self.x, self.y, self.z)
     }
